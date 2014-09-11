bbace92fe0bfdbcf3b211f02c05c9f8c17e47f13
Brandon Beecroft <brandonbeecroft@gmail.com>
2014-09-11 13:18:56 -0600

    creates project up to step 5
    could not get step 5 to work in simulator.

--- Score Keeper/SKViewController.m
+++ Score Keeper/SKViewController.m
@@ -8,7 +8,7 @@
 
 #import "SKViewController.h"
 
-@interface SKViewController ()
+@interface SKViewController () <UITextFieldDelegate>
 
 @property (strong, nonatomic) UILabel *scoreLabel;
 @property (nonatomic) double stepValue;
@@ -18,13 +18,24 @@
 
 @implementation SKViewController
 
+// reusable paramenters
+CGFloat scoreViewTopPos = 60;
+CGFloat scoreViewHeight = 80;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = @"Score Keeper";
 
-    [self addScoreView:1];
+    // allocate the array!! but don't do it in your loop :) otherwise you overwrite it again and again.
+    self.scoreLabels = [[NSMutableArray alloc] init];
+
+    // add 4 views to the view
+    for (int x = 0; x < 4; x++) {
+        [self addScoreView:x];
+    }
 
 }
 
@@ -36,38 +47,80 @@
 
 - (void) addScoreView:(int)index {
 
-    UIView *scoreView = [[UIView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 100)];
-//    scoreView.backgroundColor = [UIColor lightGrayColor];
+    // main score view
+    UIView *scoreView = [[UIView alloc] initWithFrame:CGRectMake(0, scoreViewTopPos, self.view.frame.size.width, scoreViewHeight)];
 
-    // UITextView
+    // UITextField - create fields! NOT UITextViews
     UITextField *name = [[UITextField alloc] initWithFrame:CGRectMake(10, 30, 100, 40)];
-//    name.backgroundColor = [UIColor lightGrayColor];
     name.placeholder = @"Name";
 
-
-
-    self.scoreLabel = [[UILabel alloc]initWithFrame:CGRectMake(140, 28, 200, 50)];
+    self.scoreLabel = [[UILabel alloc]initWithFrame:CGRectMake(140, 30, 200, 40)];
     self.scoreLabel.text = @"0";
 
     // UISTepper
-    UIStepper *scoreStepper = [[UIStepper alloc] initWithFrame:CGRectMake(200, 38, 50, 50)];
-    [scoreStepper addTarget:self action:@selector(changeLabelText) forControlEvents:UIControlEventValueChanged];
+    UIStepper *scoreStepper = [[UIStepper alloc] initWithFrame:CGRectMake(216, 34, 50, 40)];
+    [scoreStepper addTarget:self action:@selector(changeScore:) forControlEvents:UIControlEventValueChanged];
     scoreStepper.maximumValue = 1000;
     scoreStepper.minimumValue = -1000;
+    scoreStepper.tag = index;
 
+    // add all to the scoreView and add the scoreView to the main view.
     [scoreView addSubview:name];
     [scoreView addSubview:self.scoreLabel];
     [scoreView addSubview:scoreStepper];
 
     [self.view addSubview:scoreView];
 
+    // increment height of scoreview
+    scoreViewTopPos += scoreViewHeight;
+    
+    // add the scoreLabel to the scoreLabels array
+    [self.scoreLabels addObject:self.scoreLabel];
+
+    // number of objects in array:
+    NSLog(@"Objects in array: %lu",(unsigned long)self.scoreLabels.count);
+
 }
 
-- (void)changeLabelText {
-//    self.label.text = self.stepValue;
+- (void)changeScore:(id)sender {
+
+    UIStepper *myStepper = sender;
+    NSInteger index = myStepper.tag;
+
+    // check to see if i can get a value from the UIStepper
+    NSLog(@"%f",myStepper.value);
+
+    //check to see if I get the tag from the stepper.
+    NSLog(@"Stepper tag is: %li", (long)index);
+
+    // get scoreLabel from the array created
+    UILabel *updatedScoreLabel = [self.scoreLabels objectAtIndex:index];
+
+    //check value of newlabel - STUCK HERE
+    NSLog(@"%@",updatedScoreLabel.text);
+
+    // can't assign a double value to a string. convert a char?
+    // I cheated here and looked at the solution. Was not sure how to look up in the docs my question of conversion. Realized
+    // text is associated with NSString and that I could have looked there to find the 'stringWithFormat' parameter
+    //newLabel.text = myStepper.value;
+    updatedScoreLabel.text = [NSString stringWithFormat:@"%d",(int)myStepper.value];
+
+    //print updatedScoreText
+    NSLog(@"%@",updatedScoreLabel.text);
 
 }
 
+// First attempt at making the keyboard resign. Cheated and looked at solution
+//-(void)textFieldDidEndEditing:(UITextField *)textField {
+//    textField.resignFirstResponder = YES;
+//}
+
+
+// NOTE: still couldn't get the keyboard to remove after pressing done in the simulator. :(
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
 
 /*
 #pragma mark - Navigation
